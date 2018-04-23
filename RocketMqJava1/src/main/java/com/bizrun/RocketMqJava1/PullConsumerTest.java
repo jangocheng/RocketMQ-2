dffd3249765f41327ed1e67793747026dfcd65b5
Razi Ahmad <razizhcet@gmail.com>
2018-04-23 19:41:16 +0530

    rocketmq1

---
+++ RocketMqJava1/src/main/java/com/bizrun/RocketMqJava1/PullConsumerTest.java
@@ -0,0 +1,32 @@
+package com.bizrun.RocketMqJava1;
+
+import org.apache.rocketmq.client.consumer.DefaultMQPullConsumer;
+import org.apache.rocketmq.client.consumer.PullResult;
+import org.apache.rocketmq.client.exception.MQClientException;
+import org.apache.rocketmq.common.message.MessageQueue;
+
+public class PullConsumerTest {
+    public static void main(String[] args) throws MQClientException {
+        DefaultMQPullConsumer consumer = new DefaultMQPullConsumer("please_rename_unique_group_name_5");
+        consumer.start();
+
+        try {
+            MessageQueue mq = new MessageQueue();
+            mq.setQueueId(0);
+            mq.setTopic("TopicTest3");
+            mq.setBrokerName("vivedeMacBook-Pro.local");
+
+            long offset = 26;
+
+            long beginTime = System.currentTimeMillis();
+            PullResult pullResult = consumer.pullBlockIfNotFound(mq, null, offset, 32);
+            System.out.println(System.currentTimeMillis() - beginTime);
+            System.out.println(pullResult);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        consumer.shutdown();
+    }
+}
